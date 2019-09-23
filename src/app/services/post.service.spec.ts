f507e9511094c308426e25a52043ca523b19cad1
Hugh Chiou <hughchiou@gmail.com>
2019-09-23 13:07:45 +0800

    added should do a api post to post service

--- src/app/services/post.service.spec.ts
+++ src/app/services/post.service.spec.ts
@@ -10,6 +10,8 @@ import {
 describe('PostService', () => {
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
+  const testData = { text: `test data` };
+  const apiUrl = 'https://conduit.productionready.io/api/articles';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -24,4 +26,28 @@ describe('PostService', () => {
   it('should be created', inject([PostService], (service: PostService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should do a api post', inject([PostService], (service: PostService) => {
+    service
+      .createPost({ body: `test body` })
+      .subscribe(data => expect(data).toEqual(testData));
+
+    const req = httpTestingController.expectOne(apiUrl);
+
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual({
+      article: Object({
+        title: undefined,
+        description: 'test body',
+        body: 'test body',
+        tagList: undefined
+      })
+    });
+
+    req.flush(testData);
+  }));
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 });
